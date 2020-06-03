ade647cfc89a3822f41705d5f9762793f5cd2b4c
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-02 18:38:09 -0700

    refactor code

--- src/Deck.js
+++ src/Deck.js
@@ -1,37 +1,50 @@
 import React, { Component } from "react";
+import Card from "./Card";
+const API_BASE_URL = "https://deckofcardsapi.com/api/deck/";
 
 class Deck extends Component {
   constructor(props) {
     super(props);
-    this.state = { deckId: "", cards: [] };
+    this.state = { deck: null, drawn: [] };
     this.handleClick = this.handleClick.bind(this);
   }
 
   async componentDidMount() {
-    const resp = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle`);
-    const { deck_id: deckId } = await resp.json();
-    this.setState({ deckId });
+    const resp = await fetch(`${API_BASE_URL}new/shuffle`);
+    const deck = await resp.json();
+    console.log(deck);
+    this.setState({ deck });
   }
 
   async handleClick() {
-    console.log("clicked");
-    const resp = await fetch(
-      `https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw/.`
-    );
-    const {
-      cards: [{ image }],
-    } = await resp.json();
-    this.setState((ps) => ({ cards: [...ps.cards, image] }));
+    try {
+      const deckId = this.state.deck.deck_id;
+      const resp = await fetch(`${API_BASE_URL}${deckId}/draw/`);
+      const deck = await resp.json();
+      if (!deck.success) {
+        throw new Error("No cards remaining!");
+      }
+      console.log(deck);
+      const { code, image, suit, value } = deck.cards[0];
+      this.setState((ps) => ({
+        drawn: [...ps.drawn, { code, image, name: `${value} of ${suit}` }],
+      }));
+    } catch (error) {
+      alert(error);
+    }
   }
 
   renderCards() {
-    return this.state.cards.map((card) => <img src={card} />);
+    return this.state.drawn.map((card) => (
+      <Card name={card.name} image={card.image} />
+    ));
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>GIMME A CARD</button>
+        <h1>Card Dealer</h1>
+        <button onClick={this.handleClick}>GIMMME A CARD!</button>
         <div>{this.renderCards()}</div>
       </div>
     );
